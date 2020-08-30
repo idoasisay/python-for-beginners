ba62574ddea4de73fad7f07ff0642d939fea3adb
idoasisay <ihannah@kakao.com>
2020-08-30 10:01:44 +0000

    rench main.py

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from programmers import get_jobs as get_pro_jobs
-from stackoverflow import get_jobs as get_so_jobs
-from save import save_to_file
+from flask import Flask, render_template, request, redirect, send_file
+from programmers import get_jobs
+from exporter import save_to_file
 
-programmers_jobs = get_pro_jobs()
-stackoverflow_jobs = get_so_jobs()
-jobs = programmers_jobs + stackoverflow_jobs
+app = Flask("awesome job searching")
 
-save_to_file(jobs)
+db = {}
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/search")
+def search():
+    lang = request.args.get('lang').lower()
+    if len(lang) == 0:
+        return redirect("/")
+
+    existingJobs = db.get(lang)
+    if existingJobs:
+        jobs = existingJobs
+    else:
+        jobs = get_jobs(lang)
+        db[lang] = jobs
+    return render_template(
+        "search.html", searchingBy=lang, job_len=len(jobs), jobs=jobs)
+
+@app.route("/export")
+def export():
+  try:
+    lang = request.args.get('lang').lower()
+    if not lang:
+      raise Exception()
+    jobs = db.get(lang)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+  except:
+    return redirect('/')
+
+app.run(host="0.0.0.0")
